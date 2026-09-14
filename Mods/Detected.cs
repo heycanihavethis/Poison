4daa66ab4447c52faa4916342282b623bc07176f
Snake? <225837391+blowdryers@users.noreply.github.com>
2026-09-14 12:20:33 -0400

    update detected mods to notify user
    added extra context to initial warning; stating how user will be banned even if they are in a private, don't deploy as I don't know if this is formatted correctly.

--- Mods/Detected.cs
+++ Mods/Detected.cs
@@ -44,7 +44,7 @@ namespace Poison.Mods
             if (!allowDetected)
             {
                 LoadSoundFromURL($"{PluginInfo.ServerResourcePath}/Audio/Menu/danger.ogg", "Audio/Menu/danger.ogg", clip => Play2DAudio(clip, buttonClickVolume / 10f));
-                Prompt("The mods in this category are detected. <b>Unless you know what you're doing, you will get banned.</b> Are you sure you would like to continue?",
+                Prompt("The mods in this category are DETECTED. <b>Unless you know what you're doing, you will get banned, even in private codes. </b> Are you sure you would like to continue?",
                     () =>
                     {
                         allowDetected = true; Buttons.CurrentCategoryName = "Detected Mods";
